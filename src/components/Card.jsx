c3de2725b237b599106f408313cb18ac22d874ab
fiqiatur <fiqiatur.hrp@gmail.com>
2020-05-11 06:37:15 +0700

    react props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import React from 'react';
+import Avatar from './Avatar';
+import Detail from './Detail'
 
 
 
@@ -8,14 +9,12 @@ function Card(props) {
         <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img"
-            src={props.imgURL}
-            alt="avatar_img"
-          />
+          <Avatar imgURL={props.imgURL} />
         </div>
         <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
+          <Detail detailInfo={props.phone} />
+          <Detail detailInfo={props.email} />
+          
         </div>
       </div>
     );
