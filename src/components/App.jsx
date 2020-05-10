6cb33896848aaccd62b9f6badaabba3153d253ef
fiqiatur <fiqiatur.hrp@gmail.com>
2020-05-11 04:50:11 +0700

    react props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note'
+import React from "react";
+import Card from './Card';
+import contacts from '../contacts'
 
 function App() {
-    return <div>
-        <Header />
-        <Note />
-        <Footer />
+
+  console.log(contacts);
+  
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card 
+        name= {contacts[0].name}
+        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        phone= "+123 456 789"
+        email= "b@beyonce.com"
+      />
+      <Card 
+        name= {contacts[1].name}
+        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        phone= "+123 456 789"
+        email= "b@beyonce.com"
+      />
+       <Card 
+        name= {contacts[2].name}
+        imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        phone= "+123 456 789"
+        email= "b@beyonce.com"
+      />
     </div>
+  );
 }
 
-export default App
+export default App;
