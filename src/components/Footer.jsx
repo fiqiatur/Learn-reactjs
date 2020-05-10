fa4cae58f90aa7b394f758b728b821829f944830
fiqiatur <fiqiatur.hrp@gmail.com>
2020-05-10 20:08:50 +0700

    1 commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const year = new Date().getFullYear()
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>Learn-react&copy; {year}</p>
+        </footer>
+    )
+}
+
+export default Footer
